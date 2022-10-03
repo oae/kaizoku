d723d179085d4909bbe33ea11fd33e590558ff4a
Alperen Elhan <alperen@elhan.io>
2022-10-03 03:44:17 +0300

    chore: add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,48 @@
-import { EuiPageTemplate } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiButtonIcon,
+  EuiEmptyPrompt,
+  EuiHeader,
+  EuiHeaderSection,
+  EuiHeaderSectionItem,
+  EuiIcon,
+  EuiPageSidebar,
+  EuiPageTemplate,
+  EuiTitle,
+} from '@elastic/eui';
+import { BiBookContent } from 'react-icons/bi';
+import { FcSettings } from 'react-icons/fc';
 
 export default function IndexPage() {
   return (
     <EuiPageTemplate panelled grow>
-      <EuiPageTemplate.Sidebar sticky>Sidebar</EuiPageTemplate.Sidebar>
-      <EuiPageTemplate.Header title="Kaizoku" />
-      <EuiPageTemplate.EmptyPrompt title={<span>No spice</span>} footer={<span>Footer</span>}>
-        Empty
-      </EuiPageTemplate.EmptyPrompt>
+      <EuiPageSidebar sticky>Sidebar</EuiPageSidebar>
+      <EuiHeader>
+        <EuiHeaderSection grow={false}>
+          <EuiHeaderSectionItem border="right">
+            <EuiIcon type={BiBookContent} color="red" size="xl" />
+            <EuiTitle className="ml-1">
+              <span>Kaizoku</span>
+            </EuiTitle>
+          </EuiHeaderSectionItem>
+        </EuiHeaderSection>
+
+        <EuiHeaderSection side="right">
+          <EuiHeaderSectionItem border="right" className="mr-1">
+            <EuiButtonIcon iconType={FcSettings} iconSize="l" size="m" />
+          </EuiHeaderSectionItem>
+        </EuiHeaderSection>
+      </EuiHeader>
+      <EuiEmptyPrompt
+        icon={<EuiIcon type={BiBookContent} size="xxl" />}
+        title={<h2>No library found</h2>}
+        body={<p>To be able to add new manga, you need to create a library</p>}
+        actions={
+          <EuiButton color="primary" fill>
+            Create a Library
+          </EuiButton>
+        }
+      />
     </EuiPageTemplate>
   );
 }
