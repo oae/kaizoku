7d6fe34db6c77a5d1b274da036cf8669fef6058d
Alperen Elhan <alperen@elhan.io>
2022-11-04 03:00:05 +0300

    feat: add apprise support

--- src/server/utils/notification.ts
+++ src/server/utils/notification.ts
@@ -1,21 +1,40 @@
 import TelegramBot from 'node-telegram-bot-api';
-
-const token = process.env.TELEGRAM_TOKEN || '';
-const chatId = process.env.TELEGRAM_CHAT_ID || '';
-const disableNotification = !!parseInt(process.env.TELEGRAM_SEND_SILENTLY || '0', 10);
-const bot = new TelegramBot(token);
+import { prisma } from '../db/client';
 
 export const sendNotification = async (title: string, body: string, url?: string) => {
-  if (!token || !chatId) {
-    return;
-  }
+  const settings = await prisma.settings.findFirstOrThrow();
 
-  const message = `
+  if (settings.telegramEnabled && settings.telegramChatId && settings.telegramToken) {
+    const bot = new TelegramBot(settings.telegramToken);
+    const message = `
 <b>${title}</b>
 
 ${body}
 
 ${url || ''}
-  `;
-  await bot.sendMessage(chatId, message, { parse_mode: 'HTML', disable_notification: disableNotification });
+    `;
+    await bot.sendMessage(settings.telegramChatId, message, {
+      parse_mode: 'HTML',
+      disable_notification: settings.telegramSendSilently,
+    });
+  }
+
+  if (settings.appriseEnabled && settings.appriseHost && settings.appriseUrls.length !== 0) {
+    const appriseServiceUrl = new URL(
+      '/notify',
+      settings.appriseHost.toLowerCase().startsWith('http') ? settings.appriseHost : `http://${settings.appriseHost}`,
+    ).href;
+    await fetch(appriseServiceUrl, {
+      body: JSON.stringify({
+        urls: settings.appriseUrls,
+        title,
+        body: `${body} ${url}`,
+        format: 'html',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+  }
 };
