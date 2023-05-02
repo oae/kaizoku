eba09b06e0fd01e43e58ec853552839f1aa1cdac
Shivam Amin <xShivam.Amin@gmail.com>
2023-05-02 07:14:35 -0600

    fix: prisma migration

--- prisma/migrations/20221124200902_add_kavita/migration.sql
+++ prisma/migrations/20221124200902_add_kavita/migration.sql
@@ -2,5 +2,4 @@
 ALTER TABLE "Settings" ADD COLUMN     "kavitaEnabled" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "kavitaHost" TEXT,
 ADD COLUMN     "kavitaPassword" TEXT,
-ADD COLUMN     "kavitaUser" TEXT,
-ADD COLUMN     "kavitaLibraries" TEXT;
+ADD COLUMN     "kavitaUser" TEXT;
